eb00d1fd2a88e1a74f904d32f8e0b0731f877ff8
ainchu18 <christianfordmacamchu18@gmail.com>
2022-01-20 20:35:55 +0000

    add submit button to add expense form and redirect to expenses page

--- moneytracker/views.py
+++ moneytracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
@@ -17,12 +17,28 @@ def add_expenses(request):
         'categories': categories,
         'values': request.POST
     }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expenses.html', context)
+
     if request.method == 'POST':
         amount = request.POST['amount']
 
         if not amount:
             messages.error(request, 'Amount is required')
             return render(request, 'expenses/add_expenses.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
 
-    if request.method == 'GET':
-        return render(request, 'expenses/add_expenses.html', context)
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/add_expenses.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date, category=category, description=description)
+
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('moneytrack')
+
+
+    
