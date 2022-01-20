215c02215e6e57c4e6875e42651d4133f2035a51
ainchu18 <christianfordmacamchu18@gmail.com>
2022-01-20 20:22:08 +0000

    add add expenses html styles and inputs

--- moneytracker/urls.py
+++ moneytracker/urls.py
@@ -1,10 +1,10 @@
-from django .urls import path
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name = "moneytracker"),
-    path('add-expenses', views.add_expenses, name = "add-expense")
+    path('add-expenses', views.add_expenses, name = "add-expenses")
 ]
 
 
--- moneytracker/views.py
+++ moneytracker/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Category, Expense
+from django.contrib import messages
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories = Category.objects.all()
     return render(request, 'expenses/index.html')
 
 
+@login_required(login_url='/authentication/login')
 def add_expenses(request):
-    return render(request, 'expenses/add_expenses.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expenses.html', context)
+
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expenses.html', context)
