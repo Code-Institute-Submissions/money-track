8e0f7ba303971f9d3413a3e9a4cfd0f75e8d2929
ainchu18 <christianfordmacamchu18@gmail.com>
2022-01-21 02:30:38 +0000

    add search bar to easily go through user input data

--- moneytracker/views.py
+++ moneytracker/views.py
@@ -3,6 +3,20 @@ from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
+
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__istartswith=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
 
 @login_required(login_url='/authentication/login')
 def index(request):
@@ -79,13 +93,14 @@ def expense_edit(request, id):
         expense.owner = request.user
         expense.amount = amount
         expense.date = date
-        expense.category = category 
+        expense.category = category
         expense.description = description
 
         expense.save()
         messages.success(request, 'Expense updated successfully')
 
         return redirect('moneytracker')
+
 
 def delete_expense(request, id):
     expense = Expense.objects.get(pk=id)
